851b56906c52ee01265b2d224e15279b8cb719cb
no1memory <picture_taka@yahoo.co.jp>
2013-05-26 15:21:22 +0900

    NotImplementedSolverImplが正しく実装されていないことを示すテストケースを追加

--- practice1/src/test/java/practice/practice1/hanoi/jorsef/NotImplementedSolverImplTest.java
+++ practice1/src/test/java/practice/practice1/hanoi/jorsef/NotImplementedSolverImplTest.java
@@ -10,6 +10,10 @@ import org.junit.Test;
 import practice.practice1.hanoi.Solver;
 import practice.practice1.hanoi.Tower;
 
+/**
+ * @author araki
+ *
+ */
 public class NotImplementedSolverImplTest {
 
 	@Test
@@ -25,4 +29,19 @@ public class NotImplementedSolverImplTest {
 		assertEquals("A　から B へ円盤 1 を移動", result.get(0));
 	}
 
+	@Test
+	public void testSolve2() {
+		Tower a =  new Tower("A", new Integer[]{1,2});
+		Tower b =  new Tower("B");
+		Tower c =  new Tower("C");
+		
+		Solver solver = new NotImplementedSolverImpl();
+	
+		List<String> result = solver.solve(a, b, c, 2, new ArrayList<String>());
+		//2つの円盤を移動する際、workを利用し、最低でも3回の移動が必要
+		//（1回:from → work、2回:from→to、3回:work→to）
+		assertTrue(3 <= result.size());		
+	}	
+	
+	
 }
