5a8f7bbb3fe81187d55d9e0a7cd3be440d2ec195
jorsef <jorsef@jorsef.jp>
2013-07-15 13:11:09 +0900

    height == 0 の場合

--- practice1/src/main/java/practice/practice1/hanoi/jorsef/SolverImpl.java
+++ practice1/src/main/java/practice/practice1/hanoi/jorsef/SolverImpl.java
@@ -20,7 +20,9 @@ public class SolverImpl implements Solver {
 	@Override
 	public List<String> solve(Tower from, Tower to, Tower work, int height,
 			List<String> result) {
-		if (height == 1) {
+		if (height == 0)
+			return result;
+		else if (height == 1) {
 			Integer val = from.pop();
 			to.push(val);
 			result.add(String.format("%1$s　から %2$s へ円盤 %3$d を移動", from.getName(), to.getName(), val));
