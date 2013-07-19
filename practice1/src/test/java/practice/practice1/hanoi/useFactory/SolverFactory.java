72300960676f4a908c52674f072283d88c3b84cc
jorsef <jorsef@jorsef.jp>
2013-07-19 11:39:48 +0900

    複数の実装を同時にテスト

--- practice1/src/test/java/practice/practice1/hanoi/useFactory/SolverFactory.java
+++ practice1/src/test/java/practice/practice1/hanoi/useFactory/SolverFactory.java
@@ -1,12 +1,20 @@
 package practice.practice1.hanoi.useFactory;
 
+import java.util.Arrays;
+import java.util.List;
+
 import practice.practice1.hanoi.Solver;
-import practice.practice1.hanoi.no1memory.SolverImpl;
 
 public class SolverFactory {
+	private static Solver [] targets = {
+		new practice.practice1.hanoi.jorsef.SolverImpl(),
+		new practice.practice1.hanoi.no1memory.SolverImpl(),
+//		new practice.practice1.hanoi.kai.ka_Impl_solver(),
+		new practice.practice1.hanoi.yujim78.SolverImpl()
+	};
 
-	public static Solver getInstance() {
-		return new SolverImpl();
+	public static List<Solver> getInstance() {
+		return Arrays.asList(targets);
 	}
 
 }
