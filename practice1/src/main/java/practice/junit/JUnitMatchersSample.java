60b366bc822b453a0cb8205df062494b3cd7a953
shinta <shinta@192.168.0.5>
2013-09-19 23:37:14 +0900

    hasItem、hasItemsの説明を追加

--- practice1/src/main/java/practice/junit/JUnitMatchersSample.java
+++ practice1/src/main/java/practice/junit/JUnitMatchersSample.java
@@ -3,6 +3,8 @@ package practice.junit;
 import static org.junit.Assert.*;
 import static org.junit.matchers.JUnitMatchers.*;
 
+import java.util.ArrayList;
+
 
 import org.junit.Test;
 
@@ -10,11 +12,13 @@ public class JUnitMatchersSample {
 
 	@Test
 	public void test_assertThat() {
+		// containsString
 		{
 			String actual = "abcde";
 			assertThat(actual, containsString("a"));	// 指定した文字が含まれていればOK
 		}
 
+		// both / and
 		{
 			String actual = "abcde";
 			assertThat(actual, both(containsString("a")).and(containsString("b")).and(containsString("c")));
@@ -23,6 +27,7 @@ public class JUnitMatchersSample {
 			// allOf() でも同じ事ができるので、わかりやすい方を使用すればOK
 		}
 
+		// either / or
 		{
 			String actual = "abcde";
 			assertThat(actual, either(containsString("a")).or(containsString("x")).or(containsString("y")));
@@ -32,7 +37,28 @@ public class JUnitMatchersSample {
 		}
 
 //		everyItem
-//		hasItem
-//		hasItems
+
+		// hasItem
+		{
+			ArrayList<String> actual = new ArrayList<String>();
+			actual.add("a");
+			actual.add("b");
+			actual.add("c");
+			assertThat(actual, hasItem("b"));
+			
+			// assertArrayEquals()でArrayを指定する場合はtoArray()で取り出す必要があったが、
+			// これは、そのまま評価ができる。
+		}
+		
+		//　hasItems
+		{
+			ArrayList<String> actual = new ArrayList<String>();
+			actual.add("a");
+			actual.add("b");
+			actual.add("c");
+			assertThat(actual, hasItems("a", "b"));
+			
+			// まとめて指定可能。
+		}
 	}
 }
